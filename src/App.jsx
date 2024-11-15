b583b69f8e2617441598f71bf83498ea9e5426a6
Pradeep Kumar <111992763+Pradeep-1807@users.noreply.github.com>
2024-11-15 16:28:20 +0000

    react-calendar installed from npm

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import DatePicker from './components/DatePicker'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <h1 className="text-3xl text-center font-bold underline">
-    Hello world!
-  </h1>
-  )
+    <>
+      <h1 className="text-3xl font-bold underline text-red-400">
+        Hello world!
+      </h1>
+      <DatePicker />
+    </>
+  );
 }
 
-export default App
+export default App;
